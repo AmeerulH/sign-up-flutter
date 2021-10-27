eb23b6455032d035f829440eb26b33e56597dab3
Ameerul Zul Hadi <ameerulzhbusiness@gmail.com>
2021-10-27 17:48:14 +0800

    updated css

--- sign_up_flutter/lib/main.dart
+++ sign_up_flutter/lib/main.dart
@@ -10,9 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage (title: 'Hhello')
-    );
+    return const MaterialApp(home: MyHomePage(title: 'Hhello'));
   }
 }
 
@@ -26,148 +24,149 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-   String dropdownValue = 'Male';
-   String Name = '';
-   String Email = '';
-   String Password = '';
+  String dropdownValue = 'Male';
+  String Name = '';
+  String Email = '';
+  String Password = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign up page')
-        ),
+        appBar: AppBar(title: const Text('Sign up page')),
         body: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget> [
-              const Text('Welcome Message', style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.black,
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                const Text(
+                  'Welcome Message',
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
-                
-              ),
-              const Icon(Icons.bathroom_outlined, size: 100, color: Colors.deepPurpleAccent,),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                  hintText: 'Name',
-                  border: OutlineInputBorder(),
-                  suffixIcon: Icon(
-                    Icons.error,
+                const Icon(
+                  Icons.bathroom_outlined,
+                  size: 100,
+                  color: Colors.deepPurpleAccent,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Name',
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(
+                        Icons.error,
+                      ),
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null;
+                    },
+                    onChanged: (String? value) {
+                      setState(() {
+                        Name = value!;
+                      });
+                    },
                   ),
                 ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
-                  onChanged: (String? value) {
-                    setState(() {
-                      Name = value!;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                  hintText: 'Email',
-                  border: OutlineInputBorder(),
-                  suffixIcon: Icon(
-                    Icons.error,
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Email',
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(
+                        Icons.error,
+                      ),
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                    onChanged: (String? value) {
+                      setState(() {
+                        Email = value!;
+                      });
+                    },
                   ),
                 ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                  onChanged: (String? value) {
-                    setState(() {
-                      Email = value!;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                  hintText: 'Password',
-                  border: OutlineInputBorder(),
-                  suffixIcon: Icon(
-                    Icons.error,
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(
+                        Icons.error,
+                      ),
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                    onChanged: (String? value) {
+                      setState(() {
+                        Password = value!;
+                      });
+                    },
                   ),
                 ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                  onChanged: (String? value) {
-                    setState(() {
-                      Password = value!;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: DropdownButton (
-                  value: dropdownValue,
-                  icon: const Icon(Icons.arrow_downward),
-                  iconSize: 16,
-                  elevation: 16,
-                  style: const TextStyle(color: Colors.deepPurple),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.deepPurpleAccent,
-                  ),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      dropdownValue = newValue!;
-                    });
-                  },
-                  items: <String>['Male', 'Female', 'Others', 'Free']
-                    .map<DropdownMenuItem<String>>((String value) {
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: DropdownButton(
+                    value: dropdownValue,
+                    icon: const Icon(Icons.arrow_downward),
+                    iconSize: 16,
+                    elevation: 16,
+                    style: const TextStyle(color: Colors.deepPurple),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdownValue = newValue!;
+                      });
+                    },
+                    items: <String>['Male', 'Female', 'Others', 'Free']
+                        .map<DropdownMenuItem<String>>((String value) {
                       return DropdownMenuItem<String>(
                         value: value,
                         child: Text(value),
                       );
                     }).toList(),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: ElevatedButton(
-                  child: const Text('Submit'),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('$Name, $Email, $Password, $dropdownValue'),
-                          action: SnackBarAction(
-                            label: 'Action',
-                            onPressed: () {
-                              // Code to execute.
-                            },
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    child: const Text('Submit'),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: const Text('Successfully Signed Up!'),
+                            action: SnackBarAction(
+                              label: 'Action',
+                              onPressed: () {
+                                // Code to execute.
+                              },
+                            ),
                           ),
-                        ),
-                      );
-                    }
-                  },
-                ),
-              )
-            ],
-          )
-        )
-    );
+                        );
+                      }
+                    },
+                  ),
+                )
+              ],
+            )));
   }
 }
